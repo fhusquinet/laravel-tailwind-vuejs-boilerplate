25d777de0c7f91ff125cae3a976056a153360128
Florian Husquinet <florian.husquinet@deegital.Be>
2019-01-07 21:39:44 +0100

    ✨ Laravel mix setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,30 @@
 const mix = require('laravel-mix');
 
+require('laravel-mix-purgecss');
+require('laravel-mix-versionhash');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+function isProduction() {
+	return process.env.NODE_ENV == 'production';
+}
+
+// mix.setPublicPath('public');
+mix.webpackConfig({
+    output: {
+      publicPath: '/',
+      // it’s useful to have static filenames in development mode and hash-based in production
+		chunkFilename: isProduction() ? 'js/[name].[md5:contenthash:hex:20].js' : 'js/[name].chunk.js'
+    },
+    plugins: [
+        new CompressionPlugin()
+    ],
+    resolve: {
+        modules: [
+            path.resolve('./node_modules')
+        ]
+    }
+});
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -12,4 +37,16 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .options({
+      uglify: {
+          uglifyOptions: {
+              compress: {
+                  drop_console: true
+              }
+          }
+      }
+  })
+  .purgeCss()
+  .version()
+  .sourceMaps();
